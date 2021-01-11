942cc91065db3e098922975ec69033693671b285
Nathanael <ngreese7@ou.edu>
2021-01-11 03:28:03 -0600

    created title screen

--- main.cpp
+++ main.cpp
@@ -6,16 +6,32 @@
 using namespace std;
 
 void start();
-void game();
+void blackjack();
 char keepPlaying();
 void goodbye();
+
+enum Card : int {TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7
+                    , EIGHT = 8, NINE = 9, TEN = 10, J = 10, Q = 10, K = 10, A};
 
 
 int main() {
     start();
-    game();
-    keepPlaying();
+    goodbye();
+    //blackjack();
+    //keepPlaying();
     return 0;
+}
+void start() {
+    cout << "00000000   0          000000    000000   0      0  0        000000    000000   0      0" << endl;
+    cout << "0       0  0         0      0  0      0  0      0  0          0      0  0      0  0      0" << endl;
+    cout << "0       0  0         0      0  0      0  0      0  0          0      0  0      0  0      0" << endl;
+    cout << "0       0  0         0      0  0         0     0   0          0      0  0         0     0 " << endl;
+    cout << "00000000   0         00000000  0         000000    0          00000000  0         000000  " << endl;
+    cout << "0       0  0         0      0  0         0     0   0          0      0  0         0     0 " << endl;
+    cout << "0       0  0         0      0  0      0  0      0  0       0  0      0  0      0  0      0" << endl;
+    cout << "0       0  0         0      0  0      0  0      0  0       0  0      0  0      0  0      0" << endl;
+    cout << "00000000   00000000  0      0   000000   0      0   0000000   0      0   000000   0      0" << endl;
+
 }
 char keepPlaying() {
     char choice = 's';
