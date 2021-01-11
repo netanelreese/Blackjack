59bdc42d48e80c4465726da5bd7146b9e4c07c23
Nathanael <ngreese7@ou.edu>
2021-01-11 03:08:07 -0600

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Blackjack.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Blackjack.exe"
+  "Blackjack.exe.manifest"
+  "Blackjack.lib"
+  "Blackjack.pdb"
+  "CMakeFiles/Blackjack.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Blackjack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
